6b83604cc23939678d7ef74a5f66b60236073e5f
Gabriel Obafisile <obafisilegabriel@gmail.com>
2022-05-15 11:21:50 +0100

    added mongodb uri

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var { config } = require('dotenv');
 
 // setting up socket.io
 var http = require('http').Server(app)
 
 // setting up io and set it to req socket
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+const { sendStatus } = require('express/lib/response');
 
 app.use(express.static(__dirname))  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+
+var dbUrl = process.env.MONGOURI
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 var messages = [
     { name: "John", message: "Hello" },
@@ -22,11 +37,19 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body) 
+    var message = new Message(req.body)
+    message.save((err) => {
+        if (err) 
+                sendStatus(500, 'Internal server error');
+
+                messages.push(req.body) 
+                // notifying  users
+                io.emit('message', req.body);
+                res.sendStatus(200);
+            
+    })
     
-    // notifying  users
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    
 });
 
 io.on('connection', () => {
@@ -36,6 +59,25 @@ io.on('connection', () => {
 io.on('disconnected', () => {
 	console.log('user disconnected');
 });
+
+// mongoose.connect(dbUrl, options, (err) => {
+//     console.log('mongodb connection successful')
+// })
+
+config();
+
+const connectDB = async () => {
+	try { 
+		await mongoose.connect(process.env.MONGOURI, options);
+		console.log('MongoDB Connected');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
+
+connectDB();
 
 var server = http.listen(3010, () => {
     console.log('Listening on port', server.address().port);
